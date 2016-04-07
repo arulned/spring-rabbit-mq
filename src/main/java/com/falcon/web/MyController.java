257df15b5110d78d8b490f77b5770177b5041796
Arul <n.arulkumaran@gmail.com>
2016-04-07 16:21:52 +0530

    minor changes

--- src/main/java/com/falcon/web/MyController.java
+++ src/main/java/com/falcon/web/MyController.java
@@ -3,7 +3,6 @@ package com.falcon.web;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.falcon.config.AMQPConfig;
@@ -14,13 +13,15 @@ public class MyController {
 	@Autowired
 	private RabbitTemplate rabbitTemplate;
 
-	@RequestMapping(value = "/api/v1/create")
-	public String createStream(@RequestParam("type") String type) {
-		if ("fanout".equalsIgnoreCase(type)) {
-			rabbitTemplate.convertAndSend(AMQPConfig.MY_FANOUT_EXCHANGE, null, "a Message");
-		} else {
-			rabbitTemplate.convertAndSend(AMQPConfig.DIRECT_EXCHANGE, null, "a Message");
-		}
+	@RequestMapping(value = "/api/v1/fanout")
+	public String fanout() {
+		rabbitTemplate.convertAndSend(AMQPConfig.MY_FANOUT_EXCHANGE, null, String.valueOf(System.currentTimeMillis()));
+		return String.valueOf(System.currentTimeMillis());
+	}
+
+	@RequestMapping(value = "/api/v1/direct")
+	public String direct() {
+		rabbitTemplate.convertAndSend(AMQPConfig.DIRECT_EXCHANGE, null, String.valueOf(System.currentTimeMillis()));
 		return String.valueOf(System.currentTimeMillis());
 	}
 }
